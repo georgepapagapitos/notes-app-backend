525a796ee2ca9b3267c9f15aebbb33d5d4582d81
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-01 17:03:08 -0500

    uibuild

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:  ', request.path);
@@ -10,9 +14,6 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static('build'));
 app.use(requestLogger);
 
 let notes = [
