a1d061f1b1363d03c99a74a6afe1eb17c011c51a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-02 14:06:24 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Note = require('./models/note');
 
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 
+// middleware to log every request
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:  ', request.path);
@@ -13,83 +16,91 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next();
 };
-
 app.use(requestLogger);
+// end middleware
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-];
 
+// GET all notes from DB
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
-});
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
+}); // end GET
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find(note => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
+// GET note by id
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
+  })
+    .catch(error => next(error));
+}); // end GET
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0;
-  return maxId + 1;
-};
-
-app.post('/api/notes', (request, response) => {
+// POST a new note
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' });
   }
-
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
+    date: new Date()
+  })
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => response.json(savedAndFormattedNote))
+    .catch(error => next(error));
+}); // end POST
+
+// DELETE a note by id
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+}); // end DELETE
+
+// PUT update a note
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+  const note = {
+    content: body.content,
+    important: body.important
   }
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch(error => next(error));
+}); // end PUT
 
-  notes = notes.concat(note);
-  response.json(note);
-});
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-  response.status(204).end();
-});
-
+// middleware to respond to unknown endpoints
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 }
-
 app.use(unknownEndpoint);
+// end middleware
 
+// middleware to handle specific errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'invalid id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+}
+app.use(errorHandler);
+// end middleware
+
+// set up connection to server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
