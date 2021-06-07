63a0abc50c291de83ada3445298fff1587aa374b
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-07 12:39:49 -0500

    added user model

--- models/note.js
+++ models/note.js
@@ -10,6 +10,10 @@ const noteSchema = new mongoose.Schema({
     type: Date,
     required: true
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
   important: Boolean,
 });
 
