cdb25ed41f54b35a6fcd1a8769a110b7f360235a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-06-07 13:36:45 -0500

    user post working and tested

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -149,6 +149,27 @@ describe('where there is initially one user in db', () => {
     const usernames = usersAtEnd.map(user => user.username);
     expect(usernames).toContain(newUser.username);
   });
+
+  test('creation fails with proper code and message if username is already taken', async () => {
+    const usersAtStart = await usersInDb();
+
+    const newUser = {
+      username: 'admin',
+      name: 'Superuser',
+      password: 'password'
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('`username` to be unique');
+
+    const usersAtEnd = await usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
 });
 
 afterAll(() => {
